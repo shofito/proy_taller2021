193e6261e367b9c0ba568e789f3ad40a42655242
Sofía <sof2014@alu.uct.cl>
2021-11-03 16:34:27 -0300

    archivos iniciales

---
+++ elgg-plugin.php
@@ -0,0 +1,23 @@
+<?php
+return [
+
+
+        'routes' => [
+
+                'default:creador'=>[
+                        'path'=>'/creador',
+                        'resource'=>'pag_creador'
+                ],
+
+                'default:probando'=>[
+                        'path'=>'/probando',
+                        'resource'=>'proyecto12'
+                ],
+
+        ],
+
+ 
+        'theme' => [
+              //  'body-background-color' => '#000',
+        ],
+];
